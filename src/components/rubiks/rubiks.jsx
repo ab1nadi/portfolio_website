758e8e7e585d7465eb074d6f88834181e4c3d1f7
ab1nadi <abinadiswapp@gmail.com>
2023-08-10 22:01:04 -0600

    Got it working somewhat, the dataArray isn't updating correctly though between rotations so that is a problem

--- src/components/rubiks/rubiks.jsx
+++ src/components/rubiks/rubiks.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {useThree, Canvas, useFrame, useLoader} from '@react-three/fiber'
 import { TextureLoader } from 'three/src/loaders/TextureLoader'
 import { MeshBasicMaterial } from "three";
+import {getSide, rotateSide} from '../../../lib/rubiks/rotating3dArray'
 
 
 export default function Rubiks(props)
@@ -25,15 +26,23 @@ export default function Rubiks(props)
 
 
 let dataArray = [];
+let shuffleStack = []; // holds {side:string, direction:string}
+let n_shuffles = 13;
+let animation_State = "START";
+let speed = 0.9;
 
 function RubiksCube(props)
 {
 
     const boxRef = React.useRef(null);
+    const rotGroup = React.useRef(null);
 
     const [active, setActive] = useState(false);
+    const [shuffleAnimation, setShuffleAnimation] = useState(false);
+
     const state = useThree();
     const [squares, setSquares] = useState([]);
+    const [rotSquares, setRotSquares] = useState([]);
 
     const green = useLoader(TextureLoader, 'green.png');
     const black = useLoader(TextureLoader, 'black.png');
@@ -44,30 +53,30 @@ function RubiksCube(props)
     const yellow = useLoader(TextureLoader, 'yellow.png');
 
 
+    //array[y][z][x]
     
 
     useEffect(()=>{
-        for(let x = 0; x<props.w; x++)
+        for(let y = 0; y<props.w; y++)
         {
             dataArray.push([]);
-            for(let y = 0; y<props.w; y++)
+            for(let z = 0; z<props.w; z++)
             {
 
-                dataArray[x].push([]);
-                for(let z = 0; z<props.w; z++)
+                dataArray[y].push([]);
+                for(let x = 0; x<props.w; x++)
                 {
-                    console.log(x-props.w/2, y-props.w/2, z-props.w/2)
 
-                    dataArray[x][y].push
+                    dataArray[y][z].push
                     (
                         <mesh key={x.toString() + y.toString() + z.toString()} position={[x-props.w/2+props.s/2, y-props.w/2+props.s/2, z-props.w/2+props.s/2]}>
                             <boxGeometry args={[props.s,props.s,props.s]} />
-                            <meshBasicMaterial attach="material-0" map={x==props.w-1 ? yellow : black}  />  {/* px */}
-                            <meshBasicMaterial attach="material-1" map={x==0 ? blue : black}  />            {/* nx */}
-                            <meshBasicMaterial attach="material-2" map={y==props.w-1 ? white : black}  />   {/* py */}
-                            <meshBasicMaterial attach="material-3" map={y==0 ? orange : black}  />          {/* ny */}
-                            <meshBasicMaterial attach="material-4" map={z==props.w-1 ? green : black}  />   {/* pz */}
-                            <meshBasicMaterial attach="material-5" map={z==0 ? red : black}  />             {/* nz */}
+                            <meshBasicMaterial attach="material-0" map={x==props.w-1 ? yellow : black}  />  
+                            <meshBasicMaterial attach="material-1" map={x==0 ? blue : black}  />            
+                            <meshBasicMaterial attach="material-2" map={y==props.w-1 ? white : black}  />   
+                            <meshBasicMaterial attach="material-3" map={y==0 ? orange : black}  />          
+                            <meshBasicMaterial attach="material-4" map={z==props.w-1 ? green : black}  />  
+                            <meshBasicMaterial attach="material-5" map={z==0 ? red : black}  />            
                         </mesh>
                     )
 
@@ -81,18 +90,150 @@ function RubiksCube(props)
         setSquares(dataArray.flat())
     }, []);
 
-    useFrame(({}, d)=> {
-        boxRef.current.rotation.x += d*0.1;
-        boxRef.current.rotation.y += d*0.1;
+    // rotate
+    // rotates a side of the rubiks
+    // cube until it is done
+    // returns true when it has finnished rotating the rubiks 
+    // cube
+    function rotate(c, d)
+    {
+        let side = c.side
+        let direction = c.direction;
+        let pin = rotGroup.current;
+        switch(side)
+        {
+            case 0:
+            case 1:
+                {
+                    if(c.target == null)
+                        c.target = pin.rotation.x + (direction==1 ? Math.PI/2 : -1*Math.PI/2);
 
+                    pin.rotation.x+= speed* (direction==1 ? d : -1*d);
+
+                    if((direction==1 && pin.rotation.x >= c.target) || (direction==0 && pin.rotation.x<= c.target))
+                    {   
+                        pin.rotation.x=c.target;
+                        return true;
+                    };
+                }
+            break;
+            case 2:
+            case 3:
+                {   
+                    if(c.target == null)
+                        c.target = pin.rotation.z + (direction==1 ? Math.PI/2 : -1*Math.PI/2);
+                    
+                    pin.rotation.z+= speed* (direction==1 ? d : -1*d);
+
+                    if((direction==1 && pin.rotation.z >= c.target) || (direction==0 && pin.rotation.z<= c.target))
+                    {   
+                        pin.rotation.z=c.target;
+                        return true;
+                    };
+                }
+            break;
+            case 4:
+            case 5:
+                {
+                    if(c.target == null)
+                        c.target = pin.rotation.y + (direction==1 ? Math.PI/2 : -1*Math.PI/2);
+
+                    pin.rotation.y+= speed* (direction==1 ? d : -1*d);
+
+                    if((direction==1 && pin.rotation.y >= c.target) || (direction==0 && pin.rotation.y<= c.target))
+                    {   
+                        pin.rotation.y=c.target;
+                        return true;
+                    };
+                }
+            break;
+        }
+
+        return false;
+    }
+
+    function runShuffleAnimation(d)
+    {
+        switch(animation_State)
+        {
+            case "START":
+            {
+                animation_State = "FORWARD"
+            }
+            break;
+            case "FORWARD": 
+            {
+                if(shuffleStack.length == n_shuffles)
+                {
+                    animation_State = "BACKWARD";
+                    return;
+                }
+
+                // create a step for the stack
+                let side = Math.floor(Math.random()*6);
+                let direction = Math.floor(Math.random()*2);
+                shuffleStack.push({side:side, direction: direction, targetRot:null});
+
+                // select the elements from the data array so that they can
+                // be added to a group and rotated
+                let s = getSide(dataArray,side);
+
+                console.log(side);
+                
+                setRotSquares(s.side);
+                setSquares(s.other);
+
+                rotateSide(dataArray, side, direction)
+
+
+                // do the animation 
+                animation_State="FORWARD_ANIMATION";
+
+            }
+            break;
+            case "FORWARD_ANIMATION": 
+            {
+                let c = shuffleStack[shuffleStack.length-1];
+                if(rotate(c, d))
+                    animation_State = "FORWARD";
+            }
+            break;
+            case "BACKWARD":
+            {
+                
+            }
+            break;
+            case "BACKWARD_ANIMATION":
+            {
+                
+            }
+            break;
+            case "DONE":
+            {
+
+            }
+            break;
+        }
+    }
+
+    useFrame(({}, d)=> {
+        boxRef.current.rotation.x += d*0.3;
+        boxRef.current.rotation.y += d*0.3;
         
+        if(shuffleAnimation)
+            runShuffleAnimation(d);
     })
 
-   
+   useEffect(()=>
+   {
+   })
 
     return (
-        <group ref={boxRef} position={props.position}>
+        <group ref={boxRef} position={props.position} onClick={()=> setShuffleAnimation(true)}>
                 {squares}
+            <group ref={rotGroup}>
+                {rotSquares}
+            </group>
         </group>
     )
 }

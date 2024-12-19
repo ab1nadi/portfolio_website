15dec335041b208293ba5aee8760eb61fa771e0c
ab1nadi <abinadiswapp@gmail.com>
2024-12-18 18:00:14 -0700

    removed visits

--- src/App.jsx
+++ src/App.jsx
@@ -26,39 +26,14 @@ function App() {
   const contactMe = useRef(0);
 
 
-  // fetchVisits
-  // a little function that
-  // fetches the website hit count
-  // uses api-ninjas
-  let fetchVisits = async () =>
-  {
-    const response = await fetch('https://api.api-ninjas.com/v1/counter?id=portfolio_hit&hit=true', {
-      method: "GET",
-      headers: {
-        'X-Api-Key': 'ppTzbzAwLnjQ8Cq/pexpKQ==jtfnh9YxxGVoQtWv',
-      },
-      contentType: 'application/json',
-    });
 
-    return response.json()
-  }
-
-
-  useEffect( ()=>
-  {
-
-    fetchVisits().then((d)=>{
-      setVisits(d.value)
-    }).catch(e=> setVisits(0));
-    
-  }, []);
 
 
   return (
     <>
       <div className="w-full h-screen bg-gradient-to-r from-black  to-[#4A516D]  flex">
 
-        <Nav visits={visits} buttons={[{text:"Home", ref:home}, {text:"Skills", ref:skills}, {text:"Certificaions", ref:certs}, {text:"Projects", ref:projects},  {text:"Contact Me", ref:contactMe}]}/>
+        <Nav  buttons={[{text:"Home", ref:home}, {text:"Skills", ref:skills}, {text:"Certificaions", ref:certs}, {text:"Projects", ref:projects},  {text:"Contact Me", ref:contactMe}]}/>
         
         <div className="w-full h-screen overflow-x-hidden text-clip white whitespace-nowrap	flex flex-col gap-3">
           
